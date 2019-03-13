691c45c1e271f716cfbb76272cab7e14a61ee9f7
Marcin Pajkowski <marcin.pajkowski@gmail.com>
2019-03-13 20:55:03 +0100

    remove redundant dependencies, fixes BufferReader resource leak

--- src/main/java/ksr/Main.java
+++ src/main/java/ksr/Main.java
@@ -22,5 +22,7 @@ public class Main {
           }
           System.out.println();
       }
+
+      System.out.println(articles.size());
   }
 }
--- src/main/java/ksr/ReutersExtractor.java
+++ src/main/java/ksr/ReutersExtractor.java
@@ -20,35 +20,35 @@ public class ReutersExtractor {
     public ArrayList<Article> extractSgmFile(String path) throws IOException {
         var articles = new ArrayList<Article>();
 
-        var br = new BufferedReader(new FileReader(path));
-        var buffer = new StringBuilder();
+        try (var br = new BufferedReader(new FileReader(path))) {
+            var buffer = new StringBuilder();
 
-        for (var line = br.readLine();
-             line != null;
-             line = br.readLine()) {
-            if (line.contains(ARTICLE_CLOSE_TAG)) {
-                var matcher = REUTERS_PATTERN.matcher(buffer.toString());
+            for (var line = br.readLine();
+                 line != null;
+                 line = br.readLine()) {
+                if (line.contains(ARTICLE_CLOSE_TAG)) {
+                    var matcher = REUTERS_PATTERN.matcher(buffer.toString());
 
-                if (matcher.find()) {
-                    var title = matcher.group(2).replaceAll("&lt;", "<");
-                    var text = matcher.group(3).replaceAll("&lt;", "<");
+                    if (matcher.find()) {
+                        var title = matcher.group(2).replaceAll("&lt;", "<");
+                        var text = matcher.group(3).replaceAll("&lt;", "<");
 
-                    var places = new ArrayList<String>();
-                    var placesMatcher = LIST_PATTERN.matcher(matcher.group(1));
+                        var places = new ArrayList<String>();
+                        var placesMatcher = LIST_PATTERN.matcher(matcher.group(1));
 
-                    while (placesMatcher.find()) {
-                        places.add(placesMatcher.group(1));
+                        while (placesMatcher.find()) {
+                            places.add(placesMatcher.group(1));
+                        }
+
+                        articles.add(new Article(title, places, text));
                     }
 
-                    articles.add(new Article(title, places, text));
+                    // clean up the buffer
+                    buffer = new StringBuilder();
+                } else {
+                  buffer.append(line);
                 }
-
-                // clean up the buffer
-                buffer = new StringBuilder();
-                continue;
             }
-
-            buffer.append(line);
         }
 
         return articles;
